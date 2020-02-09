dd3eb061c0b637d3de6eee4de68d6cdcd6a446a5
Ivan <inyokwetrust@gmail.com>
2020-02-09 21:40:29 +0300

    added data load

--- recipes/models/utilities/reset_vocab.py
+++ recipes/models/utilities/reset_vocab.py
@@ -11,6 +11,10 @@ import sentencepiece as spm
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Librispeech Dataset creation.")
+    
+    parser.add_argument(
+        "--data_dst", help="data destination directory", default="./data_dir"
+    )
     parser.add_argument(
         "--model_file", help="data destination directory", default="./data_dir"
     )
@@ -28,6 +32,32 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
+
+    subpaths = {
+        "train": ["train-clean-100", "train-clean-360", "train-other-500", "ami-sdm-train", "ami-mdm-train", "ami-ihm-train", "ted-train", "commonvoice-train", "callhome-train", "dev-clean", "dev-other", "ami-sdm-dev", "ami-mdm-dev", "ami-ihm-dev", "ted-dev", "commonvoice-dev"],
+        "dev": ["test-clean", "test-other", "ami-sdm-test", "ami-mdm-test", "ami-ihm-test", "ted-test", "commonvoice-test", "callhome-test"],
+    }
+
+    lists_path = os.path.join(args.data_dst, "lists")
+
+    # Generating am/*
+    train_all_text = os.path.join(args.output_folder, "train.txt")
+
+    # prepare data
+    print("Preparing tokens and lexicon for acoustic model...\n", flush=True)
+    word_dict = defaultdict(set)
+    with open(train_all_text, "w") as ftext:
+        for key, names in subpaths.items():
+            for name in names:
+                with open(os.path.join(lists_path, name + ".lst"), "r") as flist:
+                    for line in flist:
+                        transcription = line.strip().split(" ")[3:]
+                        if key == "train":
+                            ftext.write(" ".join(transcription) + "\n")
+                        word_dict[key].update(transcription)
+
+    lexicon_words = sorted(word_dict["train"] | word_dict["dev"])
+
     sp = spm.SentencePieceProcessor()
     sp.Load(args.model_file)
 

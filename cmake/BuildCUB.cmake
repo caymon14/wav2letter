2853d0cff223604fc55f40897f61aeca53538c52
Ivan Anisimov <inyokwetrust@gmail.com>
2020-02-28 14:50:03 +0300

    buildcob changes

--- cmake/BuildCUB.cmake
+++ cmake/BuildCUB.cmake
@@ -4,16 +4,28 @@ set(CUB_URL https://github.com/NVlabs/cub.git)
 set(CUB_TAG c3cceac115c072fb63df1836ff46d8c60d9eb304) # release 1.8.0
 
 # Download CUB
-ExternalProject_Add(
-  CUB
-  PREFIX CUB
-  GIT_REPOSITORY ${CUB_URL}
-  GIT_TAG ${CUB_TAG}
-  BUILD_IN_SOURCE 0
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-)
+if (CUB_ROOT_DIR)
+  ExternalProject_Add(
+    CUB
+    PREFIX CUB
+    SOURCE_DIR ${CUB_ROOT_DIR}
+    BUILD_IN_SOURCE 0
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+  )
+else()
+  ExternalProject_Add(
+    CUB
+    PREFIX CUB
+    GIT_REPOSITORY ${CUB_URL}
+    GIT_TAG ${CUB_TAG}
+    BUILD_IN_SOURCE 0
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+  )
+endif()
 ExternalProject_Get_Property(CUB source_dir)
 set(CUB_SOURCE_DIR ${source_dir})
 ExternalProject_Get_Property(CUB binary_dir)
